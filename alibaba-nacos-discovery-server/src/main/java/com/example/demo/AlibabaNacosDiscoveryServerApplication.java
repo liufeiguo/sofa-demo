85c6df721c9b1e469bf1f807e16fef42aba352ca
liuguofei <771474065@qq.com>
2019-03-28 17:09:35 +0800

    nacos 的 demo

---
+++ alibaba-nacos-discovery-server/src/main/java/com/example/demo/AlibabaNacosDiscoveryServerApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+/**
+ * @author liuguofei
+ * @time 2019年3月28日下午4:03:50
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AlibabaNacosDiscoveryServerApplication {
+	
+	private static Log log = LogFactory.getLog(AlibabaNacosDiscoveryServerApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlibabaNacosDiscoveryServerApplication.class, args);
+	}
+	
+	
+	@RestController
+    static class TestController {
+        @GetMapping("/hello")
+        public String hello(@RequestParam String name) {
+            log.info("invoked name = " + name);
+            return "hello " + name;
+        }
+    }
+
+}
